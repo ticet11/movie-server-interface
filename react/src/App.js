f8e98751792d0b59ff09449a841a40f01acc6863
Brian Kozub <ticet11@gmail.com>
2020-05-15 23:42:14 -0600

    Added pages folder, which currently houses the add movie page.

--- react/src/App.js
+++ react/src/App.js
@@ -1,24 +1,26 @@
-
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from "./components/header"
-import { render } from 'react-dom';
-import Body from './Body/body'
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/header";
+// import { render } from "react-dom";
+import Body from "./Body/body";
+import AddMovie from "./pages/AddMovie";
 
 class App extends Component {
-  render() {
-  return (
-    <div className="App">
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Header />
 
-      <Header/>
-
-      <Body />
-
-    </div>
-  );
- }
+                    <Route exact path="/" />
+                    <Route exact path="/addmovie" component={AddMovie} />
+                    <Body />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
