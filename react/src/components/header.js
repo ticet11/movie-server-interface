97b954f8ad85733886b991866ddaad10c197dfbf
Chris Nickel <chris.nickel.dev@gmail.com>
2020-06-05 15:49:35 -0400

    Updated routes and links for new header

--- react/src/components/header.js
+++ react/src/components/header.js
@@ -2,35 +2,29 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export default class Header extends Component {
-
-    render() {
-        return (
-            <div className="header">
-                <Link to="/">Home</Link> |{" "}
-                <Link to="/allmovies">All Movies</Link> |{" "}
-                <Link to="/addmovie">Add a Movie</Link>
-                <nav>
-
-                 <ul>
-                 <li>
-                        <a href="#home">Home</a>
-                    </li>
-                    <li>
-                        <a href="#library">Library</a>
-                    </li>
-                    <li>
-                        <a href="#recently added">Recently Added</a>
-                    </li>
-                    <li>
-                        <a href="#watch list">Watch List</a>
-                    </li>
-                    <li>
-                        <a href="#search">Search</a>
-                    </li>
-                    
-                </ul> 
-                </nav>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className='header'>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/allmovies'>Library</Link>
+            </li>
+            <li>
+              <a href='#recently added'>Recently Added</a>
+            </li>
+            <li>
+              <a href='#watch list'>Watch List</a>
+            </li>
+            <li>
+              <Link to='/addmovie'>Search</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
   }
+}
