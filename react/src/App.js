36d9f4cd7f550ecdddf928e43c6c0aa9404bf186
Chris Nickel <chris.nickel.dev@gmail.com>
2020-06-05 14:06:32 -0400

    added allmovies route to library link on homepage

--- react/src/App.js
+++ react/src/App.js
@@ -1,33 +1,32 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { Component } from "react";
 
-
-import Header from './components/header';
-import Body from './components/Body/body';
-import AllMovies from './pages/AllMovies';
-import AddMovie from './pages/AddMovie';
-import "./Style/main.scss"
+import Header from "./components/header";
+import Body from "./components/Body/body";
+import AllMovies from "./pages/AllMovies";
+import AddMovie from "./pages/AddMovie";
+import "./Style/main.scss";
 
 class App extends Component {
-	render() {
-		return (
-			<Router>
-				<div className='App'>
-					<Header />
-					<Switch>
-						<Route exact path='/' component={Home} />
-						<Route exact path='/allmovies' component={AllMovies} />
-						<Route exact path='/addmovie' component={AddMovie} />
-					</Switch>
-				</div>
-			</Router>
-		);
-	}
+  render() {
+    return (
+      <Router>
+        <div className='App'>
+          <Header />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/allmovies' component={AllMovies} />
+            <Route exact path='/addmovie' component={AddMovie} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 const Home = () => (
-	<div>
-		<Body />
-	</div>
+  <div>
+    <Body />
+  </div>
 );
 export default App;
