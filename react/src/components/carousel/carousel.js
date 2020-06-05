99d539a45a943db6ad189d8d977ebfbe107df730
Brian Kozub <ticet11@gmail.com>
2020-06-04 22:55:45 -0600

    Limited filtering by genre on homepage.

--- react/src/components/carousel/carousel.js
+++ react/src/components/carousel/carousel.js
@@ -31,23 +31,40 @@ export default class Carousel extends React.Component {
         axios
             .get("http://localhost:3000/movies")
             .then((response) => {
-              console.log(response.data)
                 this.setState({
                     movies: response.data,
                 });
             })
+            .then()
             .catch((error) => {
                 console.error("getmovies error", error);
             });
     };
 
-    allMovieSlides = () => this.state.movies.map((movie) => {
-      return (
-          <Slide key={movie._id} index={movie._id}>
-              <Image src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.movie_poster}`} />
-          </Slide>
-      );
-  });
+    genreMovieSlides = (genre) =>
+        this.state.movies
+            .filter((movie) => movie.genre[0].name === genre)
+            .map((movie) => {
+                console.log(movie.movie_poster);
+                return (
+                    <Slide key={movie._id} index={movie._id}>
+                        <Image
+                            src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.movie_poster}`}
+                        />
+                    </Slide>
+                );
+            });
+
+    allMovieSlides = () =>
+        this.state.movies.map((movie) => {
+            return (
+                <Slide key={movie._id} index={movie._id}>
+                    <Image
+                        src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.movie_poster}`}
+                    />
+                </Slide>
+            );
+        });
 
     render() {
         return (
@@ -57,9 +74,7 @@ export default class Carousel extends React.Component {
                 naturalSlideWidth={250}
                 naturalSlideHeight={350}
             >
-                <Slider>
-                    {this.allMovieSlides()}
-                </Slider>
+                <Slider>{this.genreMovieSlides('Action')}</Slider>
                 <ButtonFirst>First</ButtonFirst>
                 <ButtonBack>Back</ButtonBack>
                 <ButtonNext>Next</ButtonNext>
