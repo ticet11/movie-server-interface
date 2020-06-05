6751370533c0dfcc46b59f30517294ff8697d412
Chris Nickel <chris.nickel.dev@gmail.com>
2020-06-05 17:49:16 -0400

    updated overview in routes and schema

--- database/models/movie.js
+++ database/models/movie.js
@@ -1,34 +1,38 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const movieSchema = new mongoose.Schema({
-    genre: {
-        type: Array,
-        required: true
-    },
-    movie_poster: {
-        type: String,
-        required: true
-    },
-    release_date: {
-        type: Date,
-        required: true
-    },
-    runtime: {
-        type: Number,
-        required: true
-    },
-    tagline: {
-        type: String,
-        required: true
-    },
-    title: {
-        type: String,
-        required: true
-    },
-    vote_average: {
-        type: Number,
-        required: true
-    }
-})
+  genre: {
+    type: Array,
+    required: true,
+  },
+  movie_poster: {
+    type: String,
+    required: true,
+  },
+  release_date: {
+    type: Date,
+    required: true,
+  },
+  runtime: {
+    type: Number,
+    required: true,
+  },
+  tagline: {
+    type: String,
+    required: false,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  vote_average: {
+    type: Number,
+    required: true,
+  },
+  overview: {
+    type: String,
+    required: true,
+  },
+});
 
-module.exports = mongoose.model('Movie', movieSchema)
+module.exports = mongoose.model("Movie", movieSchema);
